37e41d9a1a8b005e770028c41ce88e13b7f0e78b
fcancelinha <fcancelinha@protonmail.com>
2021-10-03 13:09:52 +0100

    feat(component): adds donation component to help a poor dev

--- src/components/Donations.jsx
+++ src/components/Donations.jsx
@@ -1,32 +1,56 @@
 import React from 'react'
-import { Box, Typography, Stack, Icon } from '@mui/material'
+import { Box, Typography, Stack } from '@mui/material'
+import bitcoin from '../assets/icons/bitcoin.png'
+import usdt from '../assets/icons/usdt.png'
+import ethereum from '../assets/icons/ethereum.png'
+
+const addresses = {
+    btc: 'qwdoiwdqwonew7q8w09123nkdwhjei',
+    tether: 'qwdoiwdqwonew7q8w09123nkdwhjei',
+    eth: 'qwdoiwdqwonew7q8w09123nkdwhjei',
+}
 
 
 const Donations = () => {
+
     return (
-        <Box>
-            <Stack>
-                <Box>
-                    <Typography>
-                    <Icon>
-                        <img src="../assets/icons/bitcoin.svg" alt="btc"/>
-                    </Icon>
+        <Stack sx={{ alignItems: 'center', my: 5, }} spacing={0}>
+
+            <Typography version="overline" sx={{ my: 1, fontFamily: 'Consolas', fontSize: 16 }}>
+                {"< Tip a poor dev ☕> "}
+            </Typography>
+
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <img src={bitcoin} alt="bitcoin-address" width="15" height="15" style={{ marginRight: 5 }} />
+                <Typography variant="caption" sx={{ color: '#AEAEAE', fontWeight: 'bold', fontFamily: 'monospace' }}>
+                    BTC&nbsp;-&nbsp;
+                    <Typography variant="caption" sx={{ color: '#AEAEAE', fontWeight: 'bold', fontFamily: 'monospace' }}>
+                        {addresses.btc}
                     </Typography>
-                </Box>
+                </Typography>
+            </Box>
 
-                <Box>
-                    <Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <img src={usdt} alt="usdt-address" width="15" height="15" style={{ marginRight: 5 }} />
+                <Typography variant="caption" sx={{ color: '#AEAEAE', fontWeight: 'bold', fontFamily: 'monospace' }}>
+                    USDT - {addresses.tether}
+                </Typography>
+            </Box>
 
-                    </Typography>
-                </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <img src={ethereum} alt="ethereum-address" width="15" height="15" style={{ marginRight: 5 }} />
+                <Typography variant="caption" sx={{ color: '#AEAEAE', fontWeight: 'bold', fontFamily: 'monospace' }}>
+                    ETH - {addresses.eth}
+                </Typography>
+            </Box>
 
-                <Box>
-                    <Typography>
+            <Box sx={{mt: 2}}>
+                <a href="github.com/fcancelinha" target="_blank">
+                    <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" width="80" height="auto" alt="github" style={{borderRadius: 5}}/>
+                </a>
+            </Box>
 
-                    </Typography>
-                </Box>
-            </Stack>
-        </Box>
+        </Stack>
     )
 }
 
