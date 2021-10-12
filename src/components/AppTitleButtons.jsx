8de2e576c4859ae96cfc994e0bc38c2ab6defb50
fcancelinha <fcancelinha@protonmail.com>
2021-10-12 23:22:05 +0100

    feat: adds calculation mode

--- src/components/AppTitleButtons.jsx
+++ src/components/AppTitleButtons.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import Box from '@mui/system/Box'
-import Button from '@mui/material/Button'
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
-import Brightness4Icon from '@mui/icons-material/Brightness4'
-import MenuIcon from '@mui/icons-material/Menu'
+import React from 'react';
+import Box from '@mui/system/Box';
+import Button from '@mui/material/Button';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import AppsIcon from '@mui/icons-material/Apps';
 
 const AppTitleButtons = () => {
 
     const style = {
         minWidth: 4,
-        height: 40,
-        borderRadius: 40,
-        width: 42,
-    }
+        height: 35,
+        width: 43,
+    };
 
 
     return (
         <Box sx={{ display: 'flex' }}>
-            <Button variant="outlined" size="small" style={{...style, marginRight: 30 }}>
-                <AttachMoneyIcon />
-            </Button>
 
-            <Button variant="outlined" size="small" style={{ ...style, height: 40 }}>
+            <Button variant="outlined" size="small" style={{ ...style, marginRight: 30 }}>
                 <Brightness4Icon />
             </Button>
 
-            <Button variant="outlined" size="small" style={{ ...style, marginLeft: 30 }}>
-                <MenuIcon />
+            <Button variant="outlined" size="small" style={{ ...style }}>
+                <AppsIcon />
             </Button>
+
+            <Button variant="outlined" size="small" style={{ ...style, marginLeft: 30}}>
+                <AttachMoneyIcon />
+            </Button>          
         </Box>
-    )
-}
+    );
+};
 
 export default AppTitleButtons;
--- src/components/ValueInput.jsx
+++ src/components/ValueInput.jsx
@@ -3,14 +3,17 @@ import TextField from '@mui/material/TextField'
 import Stack from '@mui/material/Stack'
 import InputAdornment from '@mui/material/InputAdornment'
 import Button from '@mui/material/Button'
-import Box from '@mui/material/Box'
+import IconButton from '@mui/material/Button'
 import Tooltip from '@mui/material/Tooltip'
 import Collapse  from '@mui/material/Collapse'
 import ExpandMore from '@mui/icons-material/ExpandMore'
 import ExpandLess from '@mui/icons-material/ExpandLess'
+import CachedIcon from '@mui/icons-material/Cached';
 
 const ValueInput = ({ fields, values }) => {
     const [collapsed, setCollapsed] = useState(false)
+    const [mode, setMode] = useState(false)
+    const [btcEquiv, setBtcEquiv] = useState(0.8372837)
 
     const investmentFee = `Investment Fee: ${values.investmentFee.toFixed(2)} $`
     const sellingFee = `Selling Fee: ${values.exitFee.toFixed(2)} $`
@@ -23,10 +26,18 @@ const ValueInput = ({ fields, values }) => {
             <TextField
                 {...fields.coinAmount}
                 color="secondary"
-                sx={{ mt: 1 }}
-                helperText="≈ 0.121231 BTC"
-                InputProps={{ startAdornment: <InputAdornment position="start"> ₿ </InputAdornment> }}
-                label="Coin Amount"
+                sx={{ mt: 1, width: 227 }}
+                helperText={`≈ ${btcEquiv} BTC`}
+                InputProps={{
+                    startAdornment: <InputAdornment position="start">{mode ? '₿' : '$' }</InputAdornment>,
+                    endAdornment: 
+                    <Tooltip title="Change between fiat investment and token amount" placement="top" sx={{mt: 1}}>
+                        <IconButton onClick={() => { setMode(!mode) }} variant="secondary" sx={{ minWidth: 4, width: 4 }}>
+                            <CachedIcon position="end" />
+                        </IconButton>
+                    </Tooltip>
+                }}
+                label={mode ? 'Investment Amount' : 'Coin Amount'}
                 variant="outlined"
             />
 
@@ -50,7 +61,7 @@ const ValueInput = ({ fields, values }) => {
 
 
             <Tooltip title="Fees" placement="top">
-                <Button variant="outlined" color="secondary" size="small" style={{ minWidth: 2, width: 40, height: 40, borderRadius: 40 }} onClick={() => { setCollapsed(!collapsed); }} >
+                <Button variant="outlined" color="secondary" size="small" style={{ minWidth: 2, width: 40, height: 25 }} onClick={() => { setCollapsed(!collapsed); }} >
                     {collapsed ? <ExpandLess size="large" /> : <ExpandMore size="large" />}
                 </Button>
             </Tooltip>
